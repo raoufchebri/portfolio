adc9bbe8e87c36d305ef5f9d88b1d1dfd900bb86
Raouf Chebri <raouf@chebri.com>
2020-04-03 13:59:00 +0100

    CI Test

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -2,7 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
+  let configuration = {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
@@ -27,5 +27,10 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false
-  });
+  };
+  // when you are on the CI Server
+  if (process.env.CI_SERVER) {
+    configuration.browsers = ['ChromeHeadless'];
+  }
+  config.set(configuration);
 };
